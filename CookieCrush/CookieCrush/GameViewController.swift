0673ef485ef51e628d48c90350b59031b42e46ea
Seab <honeyboyseab@gmail.com>
2016-11-01 15:39:56 -0400

    added boilerplate code to set up the game scene

--- CookieCrush/CookieCrush/GameViewController.swift
+++ CookieCrush/CookieCrush/GameViewController.swift
@@ -12,44 +12,34 @@ import GameplayKit
 
 class GameViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        if let view = self.view as! SKView? {
-            // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
-                // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
-                
-                // Present the scene
-                view.presentScene(scene)
-            }
-            
-            view.ignoresSiblingOrder = true
-            
-            view.showsFPS = true
-            view.showsNodeCount = true
-        }
-    }
-
-    override var shouldAutorotate: Bool {
-        return true
-    }
-
-    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-        if UIDevice.current.userInterfaceIdiom == .phone {
-            return .allButUpsideDown
-        } else {
-            return .all
-        }
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Release any cached data, images, etc that aren't in use.
-    }
-
+    var scene: GameScene!
+    
     override var prefersStatusBarHidden: Bool {
         return true
     }
+    
+    override var shouldAutorotate: Bool {
+        return true
+    }
+    
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        return [.portrait, .portraitUpsideDown]
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // configure the view
+        let skView = view as! SKView
+        skView.isMultipleTouchEnabled = false
+        
+        // create and configure the scene
+        scene = GameScene(size: skView.bounds.size)
+        scene.scaleMode = .aspectFill
+        
+        // present the scene
+        skView.presentScene(scene)
+    }
+    
+    
 }
